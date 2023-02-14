cd2cbcc2659f408b12503dc20860b07655e596f9
Bangs Ngek <70790541+BangsNgek@users.noreply.github.com>
2023-02-14 15:32:28 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -4,7 +4,7 @@
 # ==========================================
 
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/websocket"
+akbarvpn="raw.githubusercontent.com/BangsNgek/JualanSSH/main/websocket"
 
 # Getting Proxy Template
 wget -q -O /usr/local/bin/ws-nontls https://${akbarvpn}/websocket.py
