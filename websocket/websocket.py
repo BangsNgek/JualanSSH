67d5cc0f91b48a2a3b84b89392a48ccf3dc3f66b
Bangs Ngek <70790541+BangsNgek@users.noreply.github.com>
2023-02-14 15:35:23 +0700

    Update websocket.py

--- websocket/websocket.py
+++ websocket/websocket.py
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:109'
-RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <b><font color="magenta">SSH PREMIUM BANGSNGEK</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
